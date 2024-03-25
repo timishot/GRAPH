d200b8c66daa6aca51d1f5adbf2123a3502392ea
Timishot <timidotcom58@gmail.com>
2024-03-25 23:13:46 +0100

    working with enque

---
+++ ti.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+from collections import deque
+q = deque()
+q.append('a')
+q.append('b')
+q.append('c')
+print("Initial queue")
+print(q)
+print("\nElements dequeued from the queue")
+print(q.popleft())
+print(q.popleft())
+print(q.popleft())
+ 
+print("\nQueue after removing elements")
+print(q)
